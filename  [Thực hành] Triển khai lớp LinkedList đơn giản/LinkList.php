82fab8e233773d98e378f13ef4c8d469affda1d2
Your Name <you@example.com>
2021-05-25 14:32:35 +0700

    [Thực hành] Triển khai lớp LinkedList đơn giản

---
+++  [Thực hành] Triển khai lớp LinkedList đơn giản/LinkList.php
@@ -0,0 +1,59 @@
+<?php
+
+
+class LinkList
+{
+    private $firstNode;
+    private $lastNode;
+    private $count;
+
+    function __construct()
+    {
+        $this->firstNode = null;
+        $this->lastNode = null;
+        $this->count = 0;
+    }
+
+    public function insertFirst($data): void
+    {
+        $node = new Node($data);
+        $node->next = $this->firstNode;
+        $this->firstNode = $node;
+
+        if (is_null($this->lastNode)) {
+            $this->lastNode = $node;
+        }
+
+        $this->count++;
+    }
+    public function insertLast($data): void
+    {
+        $lastNode = $this->lastNode;
+        $node = new Node($data);
+        $lastNode->next = $node;
+        $node->next = null;
+        $this->lastNode = $node;
+
+        if (is_null($this->firstNode)) {
+            $this->firstNode = $node;
+        }
+
+        $this->count++;
+    }
+    public function totalNodes(): int
+    {
+        return $this->count;
+    }
+
+    public function readList(): array
+    {
+        $listData = [];
+        $current = $this->firstNode;
+
+        while (!is_null($current)) {
+            array_push($listData, $current->readNode());
+            $current = $current->next;
+        }
+        return $listData;
+    }
+}
